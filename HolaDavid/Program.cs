2798973329341dae107e47b57cdb45eb4c11b480
Vanidad <57409926+vanesa33@users.noreply.github.com>
2021-02-22 16:15:08 -0300

    Add files via upload

---
+++ HolaDavid/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Ejercicio_Propuesto_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string NOMBRE;
+            Console.Write("Ingrese su nombre : "); NOMBRE = Console.ReadLine();
+            if (NOMBRE == "David")
+            {
+                Console.WriteLine();
+                Console.WriteLine("HOLA");
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("NO TE CONOZCO");
+            }
+            Console.WriteLine();
+            Console.Write("Pulse una Tecla:"); Console.ReadLine();
+        }
+    }
+}
